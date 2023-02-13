df77bd001785d32d02a53cff2868586c1d33bdb2
GiveMePseudonyms <nathanhart@ntlworld.com>
2023-02-13 18:28:35 +0000

    initial

--- Project Euler Exercises/Multiples of 3 or 5.cs
+++ Project Euler Exercises/Multiples of 3 or 5.cs
@@ -23,6 +23,7 @@ Console.WriteLine("Project Euler: 3 of 5:\n");
 Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n\nFind the sum of all the multiples of 3 or 5 below 1000.");
 
 
+
 // Create integer arrays to store each number which matches our conditions
 int total = 0;
 
