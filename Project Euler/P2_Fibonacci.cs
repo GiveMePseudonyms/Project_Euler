c09f02fbd5e3e45c64406b15bce5819cd3974763
GiveMePseudonyms <nathanhart@ntlworld.com>
2023-02-13 19:17:47 +0000

    Refactored, prep for new problems

---
+++ Project Euler/P2_Fibonacci.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    class Problem2
+    {
+        static void fibinacci()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
---
+++ Project Euler/ProjectE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Project_Euler
+{
+	public class ProjectE
+	{
+		static void Main(string[] args)
+		{
+			M35 newm35 = new M35();
+		}
+	}
+}
+
